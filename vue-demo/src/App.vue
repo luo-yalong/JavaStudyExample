a4a869d55fcbfbfb66e23b2e01f2e02c32aa1a8e
luo-yalong <2192714220@qq.com>
2022-01-03 11:07:37 +0800

    添加新模块 1. 集成mybatis-plus\elasticSearch和mysql 模块

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from "@/pages/Home";
+export default {
+  name: 'App',
+  components: {Home},
+  data() {
+    return {
+      msg: 'hello,world'
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-demo/src/pages/MyItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+        <span>
+            xxxx
+        </span>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "MyItem",
+
+    }
+</script>
+
+<style scoped>
+    li{
+        text-decoration: none;
+        height: 30px;
+        line-height: 30px;
+        border: 1px solid skyblue;
+        margin: 2px;
+    }
+    li:hover{
+        background-color: orange;
+    }
+
+</style>
