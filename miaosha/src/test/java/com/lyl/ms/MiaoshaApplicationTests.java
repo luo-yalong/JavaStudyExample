dbe4c38bfd8fadd7ce0c02a73ce29eeaa9daeefa
luo-yalong <2192714220@qq.com>
2022-01-17 22:04:09 +0800

    完成秒杀功能 笔记-待整理

--- miaosha/src/test/java/com/lyl/ms/MiaoshaApplicationTests.java
+++ miaosha/src/test/java/com/lyl/ms/MiaoshaApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
---
+++ miaosha/src/test/java/com/lyl/ms/Tests/DataStructTest.java
@@ -0,0 +1,13 @@
+package com.lyl.ms.Tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeMap;
+
+public class DataStructTest {
+
+    @Test
+    void testTree() {
+        TreeMap map = new TreeMap();
+    }
+}
